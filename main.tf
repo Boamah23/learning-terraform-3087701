69fa105ed6179c4e4c3729db10e8f98230c3d1d7
Boamah23 <38210637+Boamah23@users.noreply.github.com>
2025-01-08 12:59:02 +0000

    rename security_groups in autoscaler

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "autoscaling" {
 
   vpc_zone_identifier       = module.web_vpc.public_subnets
   target_group_arns         = module.web_alb.target_group_arns
-  security_group_ids        = [module.web_sg.security_group_id]
+  security_groups           = [module.web_sg.security_group_id]
 
   image_id                  = data.aws_ami.app_ami.id
   instance_type             = var.instance_type
