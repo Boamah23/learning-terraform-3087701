3239b52d53a7aa206a102679dab44329533baf89
Boamah23 <38210637+Boamah23@users.noreply.github.com>
2025-01-09 11:22:33 +0000

    update target groups

--- main.tf
+++ main.tf
@@ -60,36 +60,25 @@ module "web_alb" {
   subnets = module.web_vpc.public_subnets
   security_groups = [module.web_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
+  listeners = [
+    {
       port     = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
       forward = {
-        target_group_key = "ex-instance"
+        target_group_key = 0
       }
     }
-  }
+  ]
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "h1"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = "i-0f6d38a07d50d080f"
+      target_id        = "i-09616bb7c0819eee6"
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
