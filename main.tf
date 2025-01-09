56b9b1f47810f4c45fcb7e6ca7356a80f2dc70e2
Boamah23 <38210637+Boamah23@users.noreply.github.com>
2025-01-09 10:58:51 +0000

    amend alb

--- main.tf
+++ main.tf
@@ -61,21 +61,35 @@ module "web_alb" {
   security_groups = [module.web_sg.security_group_id]
 
   listeners = {
-    listener_1 = {
+    ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
-      target_group_index = 0
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
     }
   }
 
-  target_groups = [
-    {
-      name_prefix      = "web-"
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      target_id        = "i-0f6d38a07d50d080f"
     }
-  ]
+  }
 
   tags = {
     Environment = "dev"
