f8f83fb189578f04669eec9d007ef1fa0e36b601
Boamah23 <38210637+Boamah23@users.noreply.github.com>
2025-01-10 10:11:58 +0000

    fix object typo

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "instance_type" {
 
 variable "ami_filter" {
     description = "Name filter and owner for AMI"
-    type = objec({
+    type = object ({
         name = string
         owner = string
     })
